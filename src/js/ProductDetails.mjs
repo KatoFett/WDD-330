bb39311cd03eb2812b6c389bd83a4ae7a39ffa19
Aaron Fox <foxtwo@byui.edu>
2024-05-01 19:39:01 -0600

    Week 2 Team

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,77 @@
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+const CART_KEY = "so-cart";
+
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
+  async init() {
+    this.product = await this.dataSource.findProductById(this.productId);
+    this.renderProductDetails("main");
+  }
+  addToCart() {
+    const currentCart = getLocalStorage(CART_KEY) || [];
+    currentCart.push(this.product);
+    setLocalStorage(CART_KEY, currentCart);
+  }
+  renderProductDetails(selector) {
+    const container = document.querySelector(selector);
+
+    // section
+    const section = document.createElement("section");
+    section.classList.add("product-detail");
+    
+    // brand
+    let elm = document.createElement("h3");
+    elm.textContent = this.product.Brand.Name;
+    section.appendChild(elm);
+
+    // name
+    elm = document.createElement("h2");
+    elm.classList.add("divider");
+    elm.textContent = this.product.NameWithoutBrand;
+    section.appendChild(elm);
+
+    // img
+    elm = document.createElement("img");
+    elm.classList.add("divider");
+    elm.src = this.product.Image;
+    elm.alt = this.product.NameWithoutBrand;
+    section.appendChild(elm);
+
+    // price
+    elm = document.createElement("p");
+    elm.classList.add("product-card__price");
+    elm.textContent = `$${this.product.FinalPrice}`;
+    section.appendChild(elm);
+
+    // color
+    elm = document.createElement("p");
+    elm.classList.add("product-card__color");
+    elm.textContent = this.product.Colors[0].ColorName;
+    section.appendChild(elm);
+
+    // description
+    elm = document.createElement("p");
+    elm.classList.add("product-card__description");
+    elm.innerHTML = this.product.DescriptionHtmlSimple;
+    section.appendChild(elm);
+
+    // add to cart
+    elm = document.createElement("div");
+    elm.classList.add("product-detail__add");
+    const btn = document.createElement("button");
+    btn.id = "addToCart";
+    btn.dataset.id = this.product.Id;
+    btn.textContent = "Add to Cart";
+    btn.addEventListener('click', this.addToCart.bind(this))
+    elm.appendChild(btn);
+    section.appendChild(elm);
+    
+    // append section to container
+    container.appendChild(section);
+  }
+}
--- src/js/product.js
+++ src/js/product.js
@@ -1,21 +1,8 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam }from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs"
 
 const dataSource = new ProductData("tents");
-const CART_KEY = "so-cart";
-
-function addProductToCart(product) {
-  const currentCart = getLocalStorage(CART_KEY) || [];
-  currentCart.push(product);
-  setLocalStorage(CART_KEY, currentCart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const productId = getParam("product");
+const product = new ProductDetails(productId, dataSource);
+product.init();
