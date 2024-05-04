0ab606fee6386e926312bf767a0b303942337ffb
Aaron Fox <foxtwo@byui.edu>
2024-05-04 12:50:15 -0600

    Remove from cart feature

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,10 +1,13 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  if (cartItems) {
+  if ((cartItems || []).length > 0) {
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
     document.querySelector(".product-list").innerHTML = htmlItems.join("");
+    document
+      .querySelector(".cart-card__delete")
+      .addEventListener("click", removeFromCart);
   } else {
     document.querySelector(".product-list").innerHTML =
       "<li>Your cart is empty. But not for long, right?</li>";
@@ -25,9 +28,24 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <a href="#" class="cart-card__delete" data-id="${item.Id}">✖</a>
 </li>`;
 
   return newItem;
+}
+
+function removeFromCart() {
+  const id = this.dataset.id;
+
+  // Remove from local storage.
+  const cartItems = getLocalStorage("so-cart");
+  const cart = cartItems.filter((c) => c.Id == id);
+  const idx = cartItems.indexOf(cart);
+  cartItems.splice(idx, 1);
+  setLocalStorage("so-cart", cartItems);
+
+  // Refresh display.
+  renderCartContents();
 }
 
 renderCartContents();
