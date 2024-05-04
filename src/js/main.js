7431152e561414c4a95c8d1ec8ebc1bb500b7145
Aaron Fox <foxtwo@byui.edu>
2024-05-04 12:22:02 -0600

    Lint and format

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,9 @@ import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
-const list = new ProductListing("tents", dataSource, document.querySelector(".product-list"));
+const list = new ProductListing(
+  "tents",
+  dataSource,
+  document.querySelector(".product-list"),
+);
 list.init();
